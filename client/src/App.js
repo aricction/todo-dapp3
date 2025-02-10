607afcc99bb58b92ef41739346d6beb1db43a4b7
Govind purty <57853588+aricction@users.noreply.github.com>
2025-02-10 18:58:26 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Routes>
 
           <Route path="/" element={<LoginForm />} />
-          <Route path="/login" element={<Navigate to="/todo" />} />
+          <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/todo" element={<ShowTodoList />} />
   
