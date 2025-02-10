571de5d73790f97e089b34729187afff5ad5b7f9
Govind purty <57853588+aricction@users.noreply.github.com>
2025-02-10 19:38:19 +0530

    Update updateTodo.jsx

--- client/src/components/updateTodo.jsx
+++ client/src/components/updateTodo.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import {API} from "../config"
 export function UpdateTodo({ _id, handleClose, handleUpdate }) {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
